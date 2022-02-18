e69cb6bd173f8cd4ecaf6a8a00e19c61ecbd830a
hailin.zhou <hailin.zhou@happyelements.com>
2022-02-18 19:17:01 +0800

    修改提交

---
+++ src/main/java/com/zbin/cisp/flowable/HolidayRequest.java
@@ -0,0 +1,13 @@
+package com.zbin.cisp.flowable;
+
+import org.flowable.engine.ProcessEngineConfiguration;
+import org.flowable.engine.impl.cfg.StandaloneProcessEngineConfiguration;
+
+public class HolidayRequest {
+
+    public static void main(String[] args) {
+        ProcessEngineConfiguration cfg = new StandaloneProcessEngineConfiguration();
+        
+    }
+
+}
